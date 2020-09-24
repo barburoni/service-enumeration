beddd6dfd40b16d0f663094157786300a5ac9eff
barburoni <34612059+barburoni@users.noreply.github.com>
2020-09-24 12:09:00 +0300

    Update smtp_enum.py

--- smtp_enum.py
+++ smtp_enum.py
@@ -9,12 +9,23 @@ if len(sys.argv) != 3:
 	print "Usage: smtp.py <usernames_list.txt> <host>"
 	sys.exit(0)
 
-print "\nEnumerating SMTP users from " + sys.argv[1] + " on host " + sys.argv[2] + '\n'
+print "\033[1;32;40m" """
+
+                   __        _______   ____  ____  ___             
+   _________ ___  / /_____  / ____/ | / / / / /  |/  / ____  __  __
+  / ___/ __ `__ \/ __/ __ \/ __/ /  |/ / / / / /|_/ / / __ \/ / / /
+ (__  ) / / / / / /_/ /_/ / /___/ /|  / /_/ / /  / / / /_/ / /_/ / 
+/____/_/ /_/ /_/\__/ .___/_____/_/ |_/\____/_/  /_(_) .___/\__, /  
+                  /_/                              /_/    /____/   
+
+""" "\033[1;32;0m"
+print "\033[1;36;40m" + "Starting enumeration...\n" + "\033[1;36;0m"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connect = s.connect((sys.argv[2], 25))
 
 banner = s.recv(1024)
+
 
 with open(sys.argv[1], "r") as a_file:
         for username in a_file:
@@ -28,4 +39,4 @@ with open(sys.argv[1], "r") as a_file:
 		else:
 			print "\033[1;31;40m[-]\033[1;31;0m Invalid username: " + username.strip()
 
-print '\n' + "Done"
+print '\n' + "Done" 
